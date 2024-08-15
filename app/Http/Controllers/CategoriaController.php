386f9c0459ca5a5a01cd52037c22cca5b4d3748d
ivanna <marqueztoledoivanna@gmail.com>
2024-08-14 22:59:56 -0600

    Removed submodule sistema-de-ventas

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -19,7 +19,7 @@ class CategoriaController extends Controller
 
     if($request){
         $query=trim($request->get('searchText'));
-        $categorias=DB::table('categoria')->where('nombre','LIKE','%'.$query.'%')
+        $categorias=DB::table('categorias')->where('nombre','LIKE','%'.$query.'%')
         ->where('condicion','=','1')
         ->orderBy('idcategoria','desc')
         ->paginate(7);
